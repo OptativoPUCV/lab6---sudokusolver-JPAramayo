e79283d93e8c8905cf22ccec07c21a728d0774bd
Your Name <you@example.com>
2024-05-18 22:18:51 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "list.h"
 
 
@@ -48,10 +49,53 @@ int is_valid(Node* n){
     return 1;
 }
 
+bool first_empty(Node* n, int* pos_i, int* pos_j) {
+   int i, j;
+   for(i = 0 ; i < 9 ; i++) {
+       for(j = 0 ; j < 9 ; j++) {
+          if(n->sudo[i][j] == 0) {
+             *pos_i = i;
+             *pos_j = j;
+             return true;
+          }
+       }
+   }
+   return false;
+}
+
+bool is_in_row_col(Node* n, int pos_i, int pos_j, int k) {
+   int i,j;
+   for(i = 0 ; i < 9 ; i++) {
+      if(n->sudo[i][pos_j] == k) return false;
+      if(n->sudo[pos_i][i] == k) return false;
+   }
+}
+
+bool is_in_3x3(Node* n, int pos_i, int pos_j, int k) {
+   int i,j;
+   int lim_inf_i = pos_i - pos_i % 3;
+   int lim_inf_j = pos_j - pos_j % 3;
+
+   for(i = lim_inf_i ; i < 3  ; i++) {
+      for(j = lim_inf_j ; j < 3 ; j++) {
+         if(n->sudo[i][j] == k) return true;
+      }
+   }
+   return false;
+}
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+   List* list=createList();
+   int i,j;
+   if (first_empty(n,&i,&j)) {
+      Node* adj = createNode();
+      for(int k = 1 ; k <= 9 ; k++) {
+         if (is_in_row_col(n,i,j,k) || is_in_3x3(n,i,j,k))
+            continue;
+         adj->sudo[i][j] = k;
+      }
+   }
+   return list;
 }
 
 
